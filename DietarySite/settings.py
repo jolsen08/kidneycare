a7117dc8656de7c2f50165cec609589519396a63
jacobolsen <jacobrolsen14@gmail.com>
2022-11-28 13:33:48 -0700

    commit on 'main'

--- DietarySite/settings.py
+++ DietarySite/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'DietarySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'intex',
+    'USER': 'postgres',
+    'PASSWORD': 'password',
+    'HOST': 'localhost'
     }
-}
+}  
 
 
 # Password validation
