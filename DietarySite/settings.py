9af68c86eb923d85f5548ab0c23c606cbf42aa6f
jacobolsen <jacobrolsen14@gmail.com>
2022-11-28 15:45:23 -0700

    Merge branch 'main' of https://github.com/5kiing/realintex

--- DietarySite/settings.py
+++ DietarySite/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'DietarySite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': 'intex',
-#     'USER': 'postgres',
-#     'PASSWORD': 'password',
-#     'HOST': 'localhost'
-#     }
-# }  
+DATABASES = {
+     'default': {
+     'ENGINE': 'django.db.backends.postgresql',
+     'NAME': 'intex',
+     'USER': 'postgres',
+     'PASSWORD': 'password',
+     'HOST': 'localhost'
+     }
+ }  
 
 
 # Password validation
