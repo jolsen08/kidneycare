c6c3709ab161f56b2b25cf8820a57b40e9ec5aaa
Sam Reyes <ksamuelreyes@gmail.com>
2022-11-29 18:10:45 -0700

    commit

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import trackerPageView
-from .views import addUserFoodPageView
+# from .views import addUserFoodPageView
 
 urlpatterns = [ 
     # path("adduserfood/<int:user_id>/", addUserFoodPageView, name="adduserfood"),
