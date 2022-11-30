3301f950f22994add9236ef53b3a706e257f1d7f
Sam Reyes <ksamuelreyes@gmail.com>
2022-11-30 08:52:44 -0700

    changes

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 from .views import trackerPageView
-<<<<<<< HEAD
-=======
-# from .views import addUserFoodPageView
->>>>>>> a973b52a96a8b8b005ac86a405673315162dd21d
 
 urlpatterns = [ 
     # path("adduserfood/<int:user_id>/", addUserFoodPageView, name="adduserfood"),
