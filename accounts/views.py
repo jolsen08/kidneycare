db573de8d93f67bdd4479a485e80018fef0897a6
Sam Reyes <ksamuelreyes@gmail.com>
2022-11-29 17:24:26 -0700

    again

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-from dashboard.models import Person, FoodConsumption
+from dashboard.models import Person
 
 
 
@@ -76,10 +76,7 @@ def registerdetails(request):
 
 def pdetails(request):
     data = Person.objects.all()
-    # food = FoodConsumption.objects.all()
     context = {
-        "person": data,
-        # "food" : food
     }
     return render(request, 'accounts/pdetails.html',context)
 
